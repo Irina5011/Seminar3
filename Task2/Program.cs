7d37e1c7bf4b2916f36a5bc4b5f73b175c04ddca
Irina <89231707647@mail.ru>
2022-03-15 11:32:38 +0700

    New tasks

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая принимает на вход координаты 
+//двух точек и находит расстояние между ними в 3D пространстве.
+
+double Prompt(string message)
+{
+    Console.WriteLine(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+double x1 = Prompt("Введите A(x) => ");
+double y1 = Prompt("Введите A(y) =>");
+
+double x2 = Prompt("Введите B(x) => ");
+double y2 = Prompt("Введите B(y) =>");
+
+double x3 = x1 - x2;
+double y3 = y1 - y2;
+
+double d = Math.Sqrt(Math.Pow(x3, 2) + Math.Pow (y3, 2));
+
+Console.WriteLine($"Расстояние между точками => {d}");
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число 
+//(N) и выдаёт таблицу кубов чисел от 1 до N.
+
+double Prompt(string message)
+{
+    Console.WriteLine(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+double N = Prompt("Введите Число => ");
+
+if (N < 0) Console.WriteLine ("Число меньше нуля"); // отсекаем отрицательные числа
+
+int A = 0;
+
+while(A < N)
+{
+    A++;
+    Console.WriteLine(A*A*A + " ");
+
+}
