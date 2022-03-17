35335462c9345e1b6c77724cf049ec91538239bf
Irina <89231707647@mail.ru>
2022-03-17 15:57:12 +0700

    task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -11,13 +11,16 @@ double Prompt(string message)
 
 double x1 = Prompt("Введите A(x) => ");
 double y1 = Prompt("Введите A(y) =>");
+double z1 = Prompt("Введите A(z) =>");
 
 double x2 = Prompt("Введите B(x) => ");
 double y2 = Prompt("Введите B(y) =>");
+double z2 = Prompt("Введите B(z) =>");
 
 double x3 = x1 - x2;
 double y3 = y1 - y2;
+double z3 = z1 - z2;
 
-double d = Math.Sqrt(Math.Pow(x3, 2) + Math.Pow (y3, 2));
+double d = Math.Sqrt(Math.Pow(x3, 2) + Math.Pow (y3, 2) + Math.Pow (z3, 2));
 
 Console.WriteLine($"Расстояние между точками => {d}");
